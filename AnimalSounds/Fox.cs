a947d878dd490042c07392bd12b8c8b92df9b5e6
Constantinescu Andrei <33575385+ConstantinescuAndrei@users.noreply.github.com>
2023-07-04 20:59:38 +0300

    Add new animals (#3)

---
+++ AnimalSounds/Fox.cs
@@ -0,0 +1,9 @@
+namespace AnimalSounds;
+
+public class Fox : Animal
+{
+    public override string MakeSound()
+    {
+        return "Bark";
+    }
+}
---
+++ AnimalSounds/Horse.cs
@@ -0,0 +1,9 @@
+namespace AnimalSounds;
+
+public class Horse : Animal
+{
+    public override string MakeSound()
+    {
+        return "Neigh";
+    }
+}
